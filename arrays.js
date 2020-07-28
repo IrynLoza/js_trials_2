cc2dc5eee185b49e448abd2b3467e93755e211f0
Iryna Loza <irynloza@gmail.com>
2020-07-28 16:20:42 -0700

    Complete everyOtherItem function

--- arrays.js
+++ arrays.js
@@ -16,8 +16,19 @@ printIndices(['apple', 'berry', 'cherry'])
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  let result = [];
+
+  for (let i = 0; i < items.length; i+= 1) {
+    if ( i % 2 === 0 ) {
+      result.push(items[i])
+    }
+  }
+
+  console.log(result)
 
 }
+
+everyOtherItem(['apple', 1, 'berry', 'cherry', 5, 10])
 
 
 // 3. smallestNItems
