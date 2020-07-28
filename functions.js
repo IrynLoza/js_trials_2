96be713f26eff263553af0911c648764a1d773ec
Iryna Loza <irynloza@gmail.com>
2020-07-28 16:39:12 -0700

    Complete isHometown

--- functions.js
+++ functions.js
@@ -4,12 +4,16 @@
 // 1. isHometown
 
 // Define your function here
+function isHometown(town) {
 
+    return town == 'San Francisco'
+}
+
+console.log(isHometown('Oakland'))
 
 // 2. getFullName
 
 // Define your function here
-
 
 // 3. calculateTotal
 
