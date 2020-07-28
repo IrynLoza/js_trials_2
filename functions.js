b5d1561395596718e94a5524d85da1b707d52b9d
Iryna Loza <irynloza@gmail.com>
2020-07-28 16:48:17 -0700

    Complete calculateTotal function

--- functions.js
+++ functions.js
@@ -26,6 +26,27 @@ console.log('Harry', 'Potter')
 // Define your function here
 function calculateTotal(basePrice, state, tax=0.05) {
 
+    const subtotal = basePrice * (1 + tax);
+
+    let fee = 0;
+
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;    
+    } else if (state === 'PA') {
+        fee = 2
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;    
+        } else {
+            fee = 3
+        } 
+    }
+
+    return subtotal + fee
 }
 
-console.log
+console.log(calculateTotal(50, 'CA'))
+console.log(calculateTotal(50, 'PA'))
+console.log(calculateTotal(50, 'MA'))
+console.log(calculateTotal(150, 'MA'))
+console.log(calculateTotal(50, 'NY'))
