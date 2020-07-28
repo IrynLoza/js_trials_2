068923db67dd983c4367c7f25d914f2c5c53c4c6
Iryna Loza <irynloza@gmail.com>
2020-07-28 16:14:00 -0700

    Complete printIndices function

--- arrays.js
+++ arrays.js
@@ -4,12 +4,19 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+
+  for (let i = 0; i < items.length; i+= 1) {
+    console.log(`${items[i]} ${i}`)
+  }
 }
+
+printIndices(['apple', 'berry', 'cherry'])
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+
 }
 
 
