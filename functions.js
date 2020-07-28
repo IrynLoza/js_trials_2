67fc585c0b6d7f9b063bd538e1c98dc3e615a762
Iryna Loza <irynloza@gmail.com>
2020-07-28 16:42:48 -0700

    Complete getFUllName function

--- functions.js
+++ functions.js
@@ -14,7 +14,18 @@ console.log(isHometown('Oakland'))
 // 2. getFullName
 
 // Define your function here
+function getFUllName(first_name, last_name) {
+
+    return `${first_name} ${last_name}`
+}
+
+console.log('Harry', 'Potter')
 
 // 3. calculateTotal
 
 // Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+
+}
+
+console.log
