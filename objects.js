4c5aa49b59a655d924d92c05aa95664abb4d77c2
Iryna Loza <irynloza@gmail.com>
2020-07-28 17:01:00 -0700

    Complete countWords function

--- objects.js
+++ objects.js
@@ -4,8 +4,23 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  let wordCount = {}
+
+  let newPhrase = phrase.split(' ')
+
+  for (const word in newPhrase) {
+    if (newPhrase[word] in wordCount) {
+      wordCount[newPhrase[word]]+= 1
+    } else {
+      wordCount[newPhrase[word]] = 1
+    }
+  }
+
+  return wordCount
+
 }
 
+console.log(countWords('I missed my my lovely friend friend JavaScript!'))
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
