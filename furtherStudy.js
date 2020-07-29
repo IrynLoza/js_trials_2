2b2fdabb86b58ecf6c4b71df677edc54b6552ef1
Iryna Loza <irynloza@gmail.com>
2020-07-28 17:29:52 -0700

    Complete wordsInCommon function

--- furtherStudy.js
+++ furtherStudy.js
@@ -3,7 +3,23 @@
 
 function wordsInCommon(words1, words2) {
   // Replace this with your code
+
+  let words1Set = new Set(words1);
+  let words2Set = new Set(words2);
+
+  let result = new Set();
+
+  for (const item of words1Set) {
+    if (words2Set.has(item)) {
+      result.add(item);
+    }
+  }
+
+  return Array.from(result)
+
 }
+
+console.log(wordsInCommon(['cheese', 'cheese', 'dessert', 'creame'], ['dessert', 'dessert', 'creame']))
 
 
 function kidsGame(names) {
