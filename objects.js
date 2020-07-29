881cb27cde03be130ccfaeb882e0371a954bba13
Iryna Loza <irynloza@gmail.com>
2020-07-28 17:18:56 -0700

    Complete getMelonsAtPrice function

--- objects.js
+++ objects.js
@@ -25,4 +25,19 @@ console.log(countWords('I missed my my lovely friend friend JavaScript!'))
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+
+  let melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }  
+
+  if (price in melonPrices) {
+      return melonPrices[price].sort()
+    } else {
+      return
+    }
 }
+
+console.log(getMelonsAtPrice(3.25))
